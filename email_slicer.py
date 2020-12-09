1c70d05fda4d321ae01f6086beb3301d0db6734e
basel <basel.nabelsi@gmail.com>
2020-12-09 22:58:45 +0100

    modify

--- email_slicer.py
+++ email_slicer.py
@@ -1,7 +1,22 @@
+import sys
+
+import socket   
+                                                             
+
+
 email_id =input("enter please your Email").strip()
 
 user_name= email_id[:email_id.index('@')]
 
 domain= email_id [email_id.index('@')+1:]
 
+ip_site = socket.gethostbyname(domain)  
+name = socket.gethostname()                                                                                     
+print (name)                                            
+print(ip_site) 
+
+sys.stdout = open("output.txt", "w")
+
 print(f"your user_name :{user_name} and your domain should be {domain}")
+
+sys.stdout.close()
